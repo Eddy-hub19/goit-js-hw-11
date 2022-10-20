315e3158b3fc059864366d84f242a92bdde4d21d
EduardSekan <sekaneduard@gmail.com>
2022-10-20 22:07:01 +0300

    add hw 11

---
+++ src/js/fetchAPI.js
@@ -0,0 +1,23 @@
+import axios from 'axios';
+
+const BASE_URL = 'https://pixabay.com';
+const API_KEY = '9534498-01df2148594d3f0d4c2aed4f3';
+
+export async function fetchPics(name, page, perPage) {
+  const params = new URLSearchParams({
+    key: `${API_KEY}`,
+    q: name,
+    image_type: 'photo',
+    orientation: 'horizontal',
+    safesearch: 'true',
+    page: page,
+    per_page: perPage,
+  });
+
+  try {
+    const response = await axios.get(`${BASE_URL}/api/?${params}`);
+    return response;
+  } catch (error) {
+    return error;
+  }
+}
